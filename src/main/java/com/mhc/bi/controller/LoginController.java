327ffd6810c2e87a779c57bb6e6ba655a13bed2e
gongben <gongben@maihaoche.com>
2018-10-17 12:13:56 +0800

    fix

--- src/main/java/com/mhc/bi/controller/LoginController.java
+++ src/main/java/com/mhc/bi/controller/LoginController.java
@@ -1,30 +1,25 @@
 package com.mhc.bi.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.mhc.framework.support.session.anno.NoAuth;
-import com.subaru.common.util.AES;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.tomcat.util.codec.binary.Base64;
+import com.mhc.framework.support.session.auth.CurrentUserHolder;
+import com.mhc.framework.support.session.base.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpSession;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * @author baiyan
  * @date 2018/10/16
  * @description
  */
+@Slf4j
 @Controller
 public class LoginController {
+
+    @Autowired
+    CurrentUserHolder currentUserHolder;
 
     @RequestMapping("/")
     public String start() {
@@ -36,21 +31,11 @@ public class LoginController {
         return "index";
     }
 
-
-    @RequestMapping("/loginValidate")
-    public String validateView(ModelMap model, HttpSession session, @RequestParam("ct") String ct) {
-        try {
-            AES aes = null;
-            aes = new AES("N5H4PYAFYMTjZtHy");
-            String plainText = new String(aes.decrypt(Base64.decodeBase64(ct)), "UTF-8");
-            JSONObject jsonObject = JSON.parseObject(plainText);
-            String ticket = jsonObject.getString("ticket");
-            String unionId = jsonObject.getString("unionId");
-            String appId = "255792592814370816";
-        } catch (Exception ignored) {
-        }
-        return "index";
+    @ResponseBody
+    @RequestMapping("/getUser")
+    public Object getUser() {
+        User user = currentUserHolder.get(User.class);
+        return "当前用户为"+user.getName();
     }
-
 }
 
