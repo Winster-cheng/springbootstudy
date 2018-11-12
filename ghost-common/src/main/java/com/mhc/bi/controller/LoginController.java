bddeed608b3fdc6c4a91518a39dd22d092b103e1
白炎 <baiyan@maihaoche.com>
2018-11-12 18:50:27 +0800

    Update LoginController.java

--- ghost-common/src/main/java/com/mhc/bi/controller/LoginController.java
+++ ghost-common/src/main/java/com/mhc/bi/controller/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-    @GetMapping("/center/**")
+    @GetMapping("/**")
     public String center() {
         return "/center/index";
     }
